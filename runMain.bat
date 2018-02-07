9338bf39d7ac0dfd4e0da685ba43fc1a19dae76d
Alexandre Campino <alexanderluiscampino@gmail.com>
2018-02-07 12:10:31 -0800

    feat: open output file after routine
    feat: open output file after routine
    
    Added to the .bat file a command line to open the notepad with the output.txt file to open automaticallt after routine has ended.
    
    Changed files names to better suit the program intention.

--- runMain.bat
+++ runMain.bat
@@ -1,7 +1,7 @@
 echo off
 title Fetch CRM User Number File
 :: See the title at the top. And this comment will not appear in the command prompt.
-cd %cd%\Desktop\Usernumber
-python crmUserNum.py
-
+cd %cd%\Desktop\GitHub\CRM-UserNumber-Fetch
+python main.py
+start notepad "output.txt"
 pause
